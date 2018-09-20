fbcc1b14aad92c1205d35b159cfbd6d332f0df95
Tejus Patel <tejus@wirelessprv-10-193-0-5.near.illinois.edu>
2018-09-19 20:18:47 -0500

    crude copy of greedy search

--- mp1-code/search.py
+++ mp1-code/search.py
@@ -39,20 +39,23 @@ def bfs(maze):
     num_states_explored = 0;
 
     q = Queue()
-    q.append(cur_pos)
+    q.put(cur_pos)
     visited[cur_pos[0]][cur_pos[1]] = True
     num_states_explored++
     #possibly use a queue to check which nodes we have visited
     #use
+    if(maze.isObjective(cur_pos)):
+        return(visited,num_states_explored)
+
     while(!q.empty()):
         
-        cur_pos = q.pop()
+        cur_pos = q.get()
         neighbors = maze.getNeighbors(cur_pos)
 
         #since it is bfs need to visit one entire layer first then move onto the next layer
         for i in neighbors:
             if visited[i[0]][i[1]] == False:
-                q.append(i)
+                q.put(i)
                 visited[i[0]][i[1]] = True
                 num_states_explored++
                 if(maze.isObjective(i)):
@@ -73,10 +76,46 @@ def dfs(maze):
 def greedy(maze):
     # TODO: Write your code here
     # return path, num_states_explored
+    # use the manhattan distance from the current position to the goal as the heuristic function 
+    # will need to use a priority queue
+
+    cur_pos = maze.getStart() # is characterized as a tuple(row,column)
+    dimensions = maze.getDimensions() # returns num of row,columns
+    visited[dimensions[0]][dimensions[1]] = [False]
+    num_states_explored = 0;
+    priority_que = queue.PriorityQueue()
+    distance = heuristic(maze,cur_pos)
+    priority_que.put([distance,cur_pos])
+    visited[cur_pos[0]][cur_pos[1]] = True
+
+    if(maze.isObjective(location)):
+        return(visited,num_states_explored)
+
+    while(!priority_que.empty()):
+        cur_pos = priority_que.get()
+        location = cur_pos[1]
+        visited[location[0]][location[1]] = True
+        num_states_explored++
+        neighbors = maze.getNeighbors(location)
+
+        if(maze.isObjective(location)):
+            return(visited,num_states_explored)
+
+        for i in neighbors:
+            distance = heuristic(maze,i)
+            priority_que.put([distance,i])
+
     return [], 0
 
 
 def astar(maze):
     # TODO: Write your code here
     # return path, num_states_explored
+    # use the manhattan distance from the current position to the goal as the heuristic function 
+
     return [], 0
+
+def heuristic(maze, cur_pos):
+    objectives = maze.getObjectives()
+    goal = objectives[-1]
+    return abs(goal[0] - cur_pos[0]) + abs(goal[1] - cur_pos[1])
