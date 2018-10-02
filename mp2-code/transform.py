8446fc74af804784f0d9cdd000f1c5f8a6c377da
Hsueh-Han Hu <hhu14@illinois.edu>
2018-10-01 22:50:23 -0500

    Commented transform.py

--- mp2-code/transform.py
+++ mp2-code/transform.py
@@ -45,32 +45,32 @@ def transformToMaze(arm, goals, obstacles, window, granularity):
     
     offsets = [min_max[0][0],min_max[1][0]] #min value of alpha and beta
    
-    row = (((min_max[0][1]-min_max[0][0])//granularity)+1)
+    row = (((min_max[0][1]-min_max[0][0])//granularity)+1) #get the number of rows and columns using the given formula
     column = (((min_max[1][1]-min_max[1][0])//granularity)+1)
-    input_map = [[SPACE_CHAR for i in range(column)] for j in range(row)]
+    input_map = [[SPACE_CHAR for i in range(column)] for j in range(row)] #instantiate the blank map
 
     
     for i in range(0,row): # alpha
         for j in range(0,column): # beta
             alpha = i*granularity
-            if(min_max[1][0]>=0):
+            if(min_max[1][0]>=0):   #if the min beta angle is positive, multiply by gran. and subtract the minimum beta angle
                 beta = j*granularity-min_max[1][0]
             else:
-                beta = j*granularity+min_max[1][0]
+                beta = j*granularity+min_max[1][0] #otherwise, mult. by gran. and add the min. beta angle
 
             
             arm.setArmAngle((alpha,beta,0))
-            armPos = arm.getArmPos()
+            armPos = arm.getArmPos() 
             armEnd = arm.getEnd()
             if((doesArmTouchObstacles(armPos,obstacles)) and not(doesArmTouchGoals(armEnd,goals)) or not(isArmWithinWindow(armPos,window))):
-                input_map[i][j] = WALL_CHAR
+                input_map[i][j] = WALL_CHAR #if arm touches obstacle and it's not a goal or it's not within the window
             elif(doesArmTouchGoals(armEnd,goals)):
-                input_map[i][j] = OBJECTIVE_CHAR
+                input_map[i][j] = OBJECTIVE_CHAR #if the arm touches a goal then objective_char
             
     
-    input_map[dimensions[0]//granularity][(dimensions[1]+min_max[1][1])//granularity] = START_CHAR
+    input_map[dimensions[0]//granularity][(dimensions[1]+min_max[1][1])//granularity] = START_CHAR #set start char
 
-    maze = Maze(input_map,offsets,granularity)
+    maze = Maze(input_map,offsets,granularity) #create maze
      
     return maze
 
