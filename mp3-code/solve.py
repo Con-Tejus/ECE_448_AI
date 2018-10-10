93e1a79a31f9a80ec25dda4e5ea0c4a6d9232ac9
Zach Clewell <zclewell@gmail.com>
2018-10-10 16:16:07 -0500

    prototype isSafe

--- mp3-code/solve.py
+++ mp3-code/solve.py
@@ -1,7 +1,43 @@
 import numpy as np
 
+#AC3
+def getKey(item):
+    return len(item[1])
 
 def solve(constraints):
+    possible_row = [(idx,gen_row(len(constraints[0]),x)) for idx,x in enumerate(constraints[0])]
+    sorted_possible_row = sorted(possible_row,key=getKey)
+    for i in possible_row:
+        print(i)
+    possible_col = [gen_row(len(constraints[1]),x) for x in constraints[1]]
+    rowsSoFar = []
+
+    """
+        While (not solution):
+            select most constrained row
+            add row to set of rows
+            restrict cols
+            if len one col has no option backtrack
+    """
+def isSafe(rowsSoFar, possible_col):
+    for row in rowsSoFar:
+        num_row = row[0]
+        row_val = row[1]
+        for idx,row_val in enumerate(row_val):
+            curr_col_options = possible_col[idx]
+            for idx, curr_col in curr_col_options:
+                if curr_col[num_row] != row_val:
+                    curr_col_options[idx] = None
+            #remove nones
+            if len(curr_col_options) == 0:
+                return False
+            possible_col[idx]= curr_col_options
+    return True, possible_col
+
+
+
+def restrictCols(rowsSoFar, possible_col):
+
     """
     Implement me!!!!!!!
     This function takes in a set of constraints. The first dimension is the axis
