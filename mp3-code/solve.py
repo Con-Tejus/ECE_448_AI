802105d300358955653022f92407bea5db557a11
Zach Clewell <zclewell@gmail.com>
2018-10-10 15:14:22 -0500

    Added gen_row function

--- mp3-code/solve.py
+++ mp3-code/solve.py
@@ -6,30 +6,30 @@ def solve(constraints):
     Implement me!!!!!!!
     This function takes in a set of constraints. The first dimension is the axis
     to which the constraints refer to. The second dimension is the list of constraints
-    for some axis index pair. The third demsion is a single constraint of the form 
+    for some axis index pair. The third demsion is a single constraint of the form
     [i,j] which means a run of i js. For example, [4,1] would correspond to a block
     [1,1,1,1].
-    
+
     The return value of this function should be a numpy array that satisfies all
     of the constraints.
 
 
 	A puzzle will have the constraints of the following format:
-	
-    
+
+
 	array([
-		[list([[4, 1]]), 
+		[list([[4, 1]]),
 		 list([[1, 1], [1, 1], [1, 1]]),
-         list([[3, 1], [1, 1]]), 
-		 list([[2, 1]]), 
+         list([[3, 1], [1, 1]]),
+		 list([[2, 1]]),
 		 list([[1, 1], [1, 1]])],
-        [list([[2, 1]]), 
-		 list([[1, 1], [1, 1]]), 
+        [list([[2, 1]]),
+		 list([[1, 1], [1, 1]]),
 		 list([[3, 1], [1, 1]]),
-         list([[1, 1], [1, 1]]), 
+         list([[1, 1], [1, 1]]),
 		 list([[5, 1]])]
 		], dtype=object)
-	
+
 	And a corresponding solution may be:
 
 	array([[0, 1, 1, 1, 1],
@@ -44,19 +44,19 @@ def solve(constraints):
 
 	array([
 	   [list([[1, 1], [1, 4], [1, 2], [1, 1], [1, 2], [1, 1]]),
-        list([[1, 3], [1, 4], [1, 3]]), 
+        list([[1, 3], [1, 4], [1, 3]]),
 		list([[1, 2]]),
-        list([[1, 4], [1, 1]]), 
+        list([[1, 4], [1, 1]]),
 		list([[2, 2], [2, 1], [1, 3]]),
-        list([[1, 2], [1, 3], [1, 2]]), 
+        list([[1, 2], [1, 3], [1, 2]]),
 		list([[2, 1]])],
        [list([[1, 3], [1, 4], [1, 2]]),
         list([[1, 1], [1, 4], [1, 2], [1, 2], [1, 1]]),
-        list([[1, 4], [1, 1], [1, 2], [1, 1]]), 
+        list([[1, 4], [1, 1], [1, 2], [1, 1]]),
 		list([[1, 2], [1, 1]]),
-        list([[1, 1], [2, 3]]), 
+        list([[1, 1], [2, 3]]),
 		list([[1, 2], [1, 3]]),
-        list([[1, 1], [1, 1], [1, 2]])]], 
+        list([[1, 1], [1, 1], [1, 2]])]],
 		dtype=object)
 
 	And a corresponding solution may be:
@@ -76,3 +76,17 @@ def solve(constraints):
     dim0 = len(constraints[0])
     dim1 = len(constraints[1])
     return np.random.randint(2, size=(dim0, dim1))
+
+def gen_row(w, s):
+    """Create all patterns of a row or col that match given runs."""
+    def gen_seg(o, sp):
+        if not o:
+            return [[0] * sp]
+        return [[0] * x + o[0] + tail
+                for x in range(1, sp - len(o) + 2)
+                for tail in gen_seg(o[1:], sp - x)]
+    # return [x[1:] for x in gen_seg([[1] * i for i in s], w + 1 - sum(s))]
+    sum = 0
+    for i in s:
+        sum+= i[0]
+    return [x[1:] for x in gen_seg([[i[1]] * i[0] for i in s], w + 1 - sum)]
