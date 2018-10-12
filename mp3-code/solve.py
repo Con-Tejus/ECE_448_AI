0818ade8aa8a7fb9ac9d3906321611983f295011
Zach Clewell <zclewell@gmail.com>
2018-10-12 16:33:03 -0500

    Working on small inputs, need AC3

--- mp3-code/solve.py
+++ mp3-code/solve.py
@@ -1,42 +1,74 @@
 import numpy as np
+import copy
 
 #AC3
 def getKey(item):
     return len(item[1])
 
-def solve(constraints):
-    possible_row = [(idx,gen_row(len(constraints[0]),x)) for idx,x in enumerate(constraints[0])]
-    sorted_possible_row = sorted(possible_row,key=getKey)
-    for i in possible_row:
-        print(i)
-    possible_col = [gen_row(len(constraints[1]),x) for x in constraints[1]]
-    rowsSoFar = []
+def format(thing):
+    sorted_thing = sorted(thing)
+    # print(sorted_thing)
+    formatted = []
+    # for row in reversed(sorted_thing):
+    #     formatted.append([x for x in reversed(row[1])])
+    for row in sorted_thing:
+        formatted.append(row[1])
+    return formatted
 
-def solve(rowsSoFar, possible_row, possible_col): #recursive
-    for row in possible_row[0]:
-        rowsSoFar.append(row)
+def solve(constraints):
+    # print(constraints[0])
+    possible_row = [(idx,gen_row(len(constraints[1]),x)) for idx,x in enumerate(constraints[0])]
+    sorted_possible_row = sorted(possible_row,key=getKey)
+    possible_col = [gen_row(len(constraints[0]),x) for x in constraints[1]]
+    # print(len(sorted_possible_row),len(possible_col))
+    for idx,curr in enumerate(possible_col):
+        if curr == []:
+            possible_col[idx] = [[0]*len(possible_col)]
+    rowsSoFar = []
+    target = len(possible_row)
+    to_be_formatted = solveR(rowsSoFar,sorted_possible_row,possible_col,target)
+    print(to_be_formatted)
+    sol = np.array(format(to_be_formatted))
+    print(sol)
+    return sol
+
+def solveR(rowsSoFar, sorted_possible_row, possible_col,target): #recursive
+    print('solveR')
+    if len(rowsSoFar) == target:
+        return rowsSoFar
+    curr_row_set = sorted_possible_row[0]
+    # print(curr_row_set)
+    num_row = curr_row_set[0]
+    # print(len(curr_row_set[1]))
+    for curr_row in curr_row_set[1]:
+        rowsSoFar.append((num_row,curr_row))
+        # print(rowsSoFar)
         ret = isSafe(rowsSoFar, possible_col)
         if ret[0]:
-            if len(rowsSoFar) == target:
-                return rowsSoFar
-            return solve(rowsSoFar, possible_row[1:],ret[1])
-        else:
-            
+            child_ret = solveR(copy.deepcopy(rowsSoFar),sorted_possible_row[1:],ret[1],target)
+            if child_ret[0]:
+                return child_ret
         rowsSoFar = rowsSoFar[:-1]
+    return False, None
 
-def isSafe(rowsSoFar, possible_col):
+
+def isSafe(rowsSoFar, given_possible_col):
+    possible_col = copy.deepcopy(given_possible_col)
+    # print(possible_col)
+    if len(rowsSoFar) == 0:
+        return True, possible_col
     for row in rowsSoFar:
         num_row = row[0]
-        row_val = row[1]
-        for idx,row_val in enumerate(row_val):
-            curr_col_options = possible_col[idx]
-            for idx, curr_col in curr_col_options:
+        curr_row = row[1]
+        for row_idx, row_val in enumerate(curr_row):
+            curr_col_options = possible_col[row_idx]
+            for col_idx, curr_col in enumerate(curr_col_options):
                 if curr_col[num_row] != row_val:
-                    curr_col_options[idx] = None
-            #remove nones
+                    curr_col_options[col_idx] = None
+            curr_col_options = [x for x in curr_col_options if x != None]
             if len(curr_col_options) == 0:
-                return False
-            possible_col[idx]= curr_col_options
+                return False, None
+            possible_col[row_idx] = curr_col_options
     return True, possible_col
 
 
@@ -129,4 +161,6 @@ def gen_row(w, s):
     sum = 0
     for i in s:
         sum+= i[0]
+
+
     return [x[1:] for x in gen_seg([[i[1]] * i[0] for i in s], w + 1 - sum)]
