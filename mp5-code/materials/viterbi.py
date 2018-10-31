908888e084fa4e34db9514c1eb9dd4b3150d6e98
Zach Clewell <zclewell@gmail.com>
2018-10-31 14:45:20 -0500

    viterbi working 0.65

--- mp5-code/materials/viterbi.py
+++ mp5-code/materials/viterbi.py
@@ -17,7 +17,7 @@ files and classes when code is run, so be careful to not modify anything else.
 TODO: implement the baseline algorithm.
 input:  training data (list of sentences, with tags on the words)
         test data (list of sentences, no tags on the words)
-output: list of sentences, each sentence is a list of (word,tag) pairs. 
+output: list of sentences, each sentence is a list of (word,tag) pairs.
         E.g., [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
 '''
 def baseline(train, test):
@@ -41,14 +41,14 @@ def baseline(train, test):
     type_check = 0
     word_type = ''
     sentence_list = []
-    
+
     for i in train:
         for j in i:
             if type[j[1]].get(j[0],0) == 0:
                 type[j[1]][j[0]] = 1
             else:
                 type[j[1]][j[0]] = 1 + type[j[1]][j[0]]
-            
+
     #test[i] gives the sentence
     #test[i][j] gives the word
     for count,sentence in enumerate(test):
@@ -74,35 +74,110 @@ output: list of sentences with tags on the words
 '''
 def viterbi(train, test):
     predicts = []
-    type = {}
-    type['DET'] = {}
-    type['NOUN'] = {}
-    type['VERB'] = {}
-    type['ADJ'] = {}
-    type['ADP'] = {}
-    type['.'] = {}
-    type['ADV'] = {}
-    type['CONJ'] = {}
-    type['PRT'] = {}
-    type['PRON'] = {}
-    type['NUM'] = {}
-    type['X'] = {}
+    # type = {}
+    # type['DET'] = {}
+    # type['NOUN'] = {}
+    # type['VERB'] = {}
+    # type['ADJ'] = {}
+    # type['ADP'] = {}
+    # type['.'] = {}
+    # type['ADV'] = {}
+    # type['CONJ'] = {}
+    # type['PRT'] = {}
+    # type['PRON'] = {}
+    # type['NUM'] = {}
+    # type['X'] = {}
     type_list = ['DET','NOUN','VERB','ADJ','ADP','.', 'ADV', 'CONJ', 'PRT','PRON','NUM','X']
     # j[1] gives the type of the word
     # j[0] gives the word
     type_check = 0
     word_type = ''
     sentence_list = []
-    
+
     initialCount = 0
     transitionCount = 0
     emissionCount = 0
 
+    tranProb = {}
+    tranProb['DET'] = {}
+    tranProb['NOUN'] = {}
+    tranProb['VERB'] = {}
+    tranProb['ADJ'] = {}
+    tranProb['ADP'] = {}
+    tranProb['.'] = {}
+    tranProb['ADV'] = {}
+    tranProb['CONJ'] = {}
+    tranProb['PRT'] = {}
+    tranProb['PRON'] = {}
+    tranProb['NUM'] = {}
+    tranProb['X'] = {}
+    initProb = {}
+    emitProb = {}
+    emitProb['DET'] = {}
+    emitProb['NOUN'] = {}
+    emitProb['VERB'] = {}
+    emitProb['ADJ'] = {}
+    emitProb['ADP'] = {}
+    emitProb['.'] = {}
+    emitProb['ADV'] = {}
+    emitProb['CONJ'] = {}
+    emitProb['PRT'] = {}
+    emitProb['PRON'] = {}
+    emitProb['NUM'] = {}
+    emitProb['X'] = {}
 
-   
+    for i in train:
+        prev_tag = ''
+        for j_id,j in enumerate(i):
+            if j_id == 0:
+                try:
+                    initProb[j[1]] += 1
+                except KeyError:
+                    initProb[j[1]] = 1
+                # initialCount += 1
+            else:
+                try:
+                    tranProb[(prev_tag, j[1])] += 1
+                except KeyError:
+                    tranProb[(prev_tag, j[1])] = 1
+                # transitionCount += 1
+            prev_tag = j[1]
+            try:
+                emitProb[j[1]][j[0]] += 1
+            except KeyError:
+                emitProb[j[1]][j[0]] = 1
+            # emissionCount += 1
+
+    for count,sentence in enumerate(test):
+        sentence_list = []
+        prev_tag = ''
+        for word_id, word in enumerate(sentence):
+            type_check = 0
+            word_type = ''
+            if word_id == 0:
+                for j in type_list:
+                    if initProb[j] * emitProb[j].get(word,0) > type_check:
+                        word_type = j
+                        type_check = initProb[j] * emitProb[j].get(word,0)
+            else:
+                for j in type_list:
+                    if tranProb.get((prev_tag,j),0) * emitProb[j].get(word,0) > type_check:
+                        word_type = j
+                        type_check = tranProb.get((prev_tag,j),0) * emitProb[j].get(word,0)
+            prev_tag = word_type
+            word_out = (word,word_type)
+            sentence_list.append(word_out)
+        predicts.append(sentence_list)
+    return predicts
+
+
+
+
+
     #Initial probability: check first tag of each sentence
-    #Transition probability: while going through sentence, keep copy of current tag, move to next one 
-    
-    
+    #Transition probability: while going through sentence, keep copy of current tag, move to next one
+    #Emission probability How often does tag t yield word w?
+
+
 
     return predicts
